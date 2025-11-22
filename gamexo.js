c9f61012c35cb3be77883e9030a4691d453ab14c
xo2xo2 <aliahmed545454p@gmail.com>
2025-11-22 19:03:58 +0300

    commit & push

--- gamexo.js
+++ gamexo.js
@@ -25364,7 +25364,7 @@ _0x166b7d.prototype.Jf = function () {
 
                 _0x36537b.endFill();
 
-                _0x36537b.lineStyle(1.5, #ffd000ff);
+                _0x36537b.lineStyle(1.5, 0xFFFFFF);
 
                 _0x36537b.drawCircle(0, 0, this.kh);
 
